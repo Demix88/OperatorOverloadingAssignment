a2fcd7d1329b1fcecc6cedf5bad3d75faa8850ec
Demix88 <skrasnauskis@gmail.com>
2022-03-30 14:13:07 +0100

    Add project files.

---
+++ OperatorOverloadingAssignment/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloadingAssignment
+{
+    class Program
+    {
+        public class EmployeeA
+        {
+            public string FirstName = "John";
+            public string LastName = "Wayne";
+            public int Id = 076;
+
+        }
+        public class EmployeeB
+        {
+            public string FirstName = "Sarah";
+            public string LastName = "Parker";
+            public int IdB = 986;
+
+        }
+
+        static void Main(string[] args)
+        { }
+     public static bool operator ==( EmployeeA Id ,  EmployeeB IdB)
+        {
+           
+            return Equals( Id,  IdB);
+        }
+
+        public static bool operator !=(EmployeeA Id, EmployeeB IdB)
+        {
+            return !Equals(Id, IdB);
+        }
+
+
+
+    }
+}
+
