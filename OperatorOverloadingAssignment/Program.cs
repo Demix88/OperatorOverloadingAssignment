c1c03137e404ad589f409e8ee9729a146dffb7b6
Demix88 <skrasnauskis@gmail.com>
2022-03-30 19:36:10 +0100

    updated

--- OperatorOverloadingAssignment/Program.cs
+++ OperatorOverloadingAssignment/Program.cs
@@ -8,36 +8,65 @@ namespace OperatorOverloadingAssignment
 {
     class Program
     {
-        public class EmployeeA
-        {
-            public string FirstName = "John";
-            public string LastName = "Wayne";
-            public int Id = 076;
-
-        }
-        public class EmployeeB
-        {
-            public string FirstName = "Sarah";
-            public string LastName = "Parker";
-            public int IdB = 986;
-
-        }
 
         static void Main(string[] args)
-        { }
-     public static bool operator ==( EmployeeA Id ,  EmployeeB IdB)
         {
-           
-            return Equals( Id,  IdB);
-        }
+            Employee employee1 = new Employee();
+            employee1.Id = 271;
+            employee1.FirstName = "Edward";
+            employee1.LastName = "Jones";
 
-        public static bool operator !=(EmployeeA Id, EmployeeB IdB)
+            Employee employee2 = new Employee();
+            employee2.Id = 485;
+            employee2.FirstName = "Sarah";
+            employee2.LastName = "Parker";
+            if (employee1 == employee2)
+            {
+                Console.WriteLine("Employee1 id is same as  employee2");
+            }
+            else
+            {
+                Console.WriteLine("Employee1 id is not the  same as  employee2");
+            }
+            Console.ReadLine();
+        }
+        public class Employee
         {
-            return !Equals(Id, IdB);
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+
+
+            public static bool operator ==(Employee employee1, Employee employee2)
+            {
+                if (employee1.Id.Equals(employee2.Id))
+                {
+                    return true;
+                }
+                return false;
+            }
+
+
+            public static bool operator !=(Employee employee1, Employee employee2)
+            {
+                if (!employee1.Id.Equals(employee2.Id))
+                {
+                    return true;
+                }
+                return false;
+            }
+            public override bool Equals(object obj)
+            {
+                Employee employee = obj as Employee;
+                if (employee != null)
+                {
+                    return employee.Id.Equals(this.Id);
+                }
+                return false;
+            }
         }
-
-
-
     }
 }
+
 
